15014b96a106ab48a51e23d885f5bf9b2c6ad4eb
layconjohn <layconjohn@gmail.com>
2023-07-02 17:23:29 -0300

    feat(user): endpoint create User

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,32 @@
-export class CreateUserDto {}
+import { IsEmail, IsString, Matches, MaxLength, MinLength } from "class-validator";
+import { User } from "../entities/user.entity";
+
+export class CreateUserDto extends User {
+    @IsEmail()
+    email: string;
+
+    @IsString()
+    nome: string;
+
+    @IsString()
+    @MinLength(6)
+    @MaxLength(16)
+    @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+        message: 'Senha muito fraca'
+    })
+    senha: string;
+
+    @IsString()
+    @MinLength(6)
+    @MaxLength(16)
+    @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+        message: 'Senha muito fraca'
+    })
+    confirmarSenha: string;
+
+    @IsString()
+    @Matches(/^\d{3}\.\d{3}\.\d{3}\-\d{2}$/, {
+        message: 'CPF fora do formato válido'
+    })
+    cpf: string;
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,11 +1,32 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import * as bcrypt from "bcrypt";
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(private readonly prisma: PrismaService){}
+
+  async create(createUserDto: CreateUserDto) {
+    if (createUserDto.senha !== createUserDto.confirmarSenha) {
+      throw new Error("Senha e confirmar senha devem ser iguais");
+    }
+
+    const userData = {
+      ...createUserDto,
+      confirmarSenha: undefined,
+      senha: await bcrypt.hash(createUserDto.senha, 10) 
+    };
+
+    const createdUser = await this.prisma.users.create({
+      data: userData
+    })
+
+    return {
+      ...createdUser,
+      senha: undefined
+    }
   }
 
   findAll() {
