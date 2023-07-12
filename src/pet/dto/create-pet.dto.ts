dd3998e4b725bd5a80e13912fec5da9c1dc00137
layconjohn <layconjohn@gmail.com>
2023-07-12 00:01:58 -0300

    feat(pet): validate body on dto create pet

--- src/pet/dto/create-pet.dto.ts
+++ src/pet/dto/create-pet.dto.ts
@@ -1,1 +1,30 @@
-export class CreatePetDto {}
+import { IsBoolean, IsNotEmpty, IsNumber, IsString, Matches } from "class-validator";
+import { Pet } from "../entities/pet.entity";
+
+export class CreatePetDto extends Pet {
+    @IsString()
+    @IsNotEmpty()
+    nome: string;
+
+    @IsString()
+    @IsNotEmpty()
+    raca: string;
+
+    @IsString()
+    @IsNotEmpty()
+    descricao: string;
+
+    @IsString()
+    @IsNotEmpty()
+    @Matches(/(((https?|ftp):\/\/)?([\w\-\.])+(\.)([\w]){2,4}([\w\/+=%&_\.~?\-]*))*$/)
+    imagem: string;
+
+    @IsString()
+    @IsNotEmpty()
+    @Matches(/(?:\()[0-9]{2}(?:\))\s?[0-9]{4,5}(?:-)[0-9]{4}$/)
+    contato: string;
+
+    @IsString()
+    @Matches(/(\d{2})[-.\/](\d{2})[-.\/](\d{4})/)
+    nascimento: string;
+}
--- src/pet/entities/pet.entity.ts
+++ src/pet/entities/pet.entity.ts
@@ -1,1 +1,11 @@
-export class Pet {}
+export class Pet {
+    id?: number;
+    nome: string;
+    raca: string;
+    adotado?: boolean;
+    descricao: string;
+    imagem: string;
+    contato: string;
+    nascimento: string;
+    userId?: number;
+}
