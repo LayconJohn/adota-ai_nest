cff4460523d19370a791616cc29a53c03fbb5ce8
layconjohn <layconjohn@gmail.com>
2023-07-22 16:05:01 -0300

    refactor(user): add decorator ApiBearerAuth

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -2,13 +2,14 @@ import { Controller, Get, Post, Body, Patch, Param, Delete, HttpException, HttpS
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { IsPublic } from 'src/auth/decorators/is-public.decorator';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @ApiTags('users')
+  @ApiBearerAuth()
   @Post()
   async create(@Body() createUserDto: CreateUserDto) {
     try {
